b955bc9aa64c1e02c1dd26bf538f3d5cb691d5e6
Ananda-Madhi <147123700+Ananda-Madhi@users.noreply.github.com>
2025-10-02 16:39:06 +0700

    Commit No.3 - Deleting an Item from RecyclerView by Swiping By Ananda Christopher Gunawan 00000084262

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -14,7 +14,7 @@ import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.Gender
 import androidx.appcompat.app.AlertDialog
-
+import androidx.recyclerview.widget.ItemTouchHelper
 
 class MainActivity : AppCompatActivity() {
     private val recyclerView: RecyclerView by lazy {
@@ -69,6 +69,8 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+        val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
     //This will create a pop up dialog when one of the items from the recycler view is clicked.
     private fun showSelectionDialog(cat: CatModel) {
